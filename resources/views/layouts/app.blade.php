3993edc0d2a8a95b39fc45e16a36fcce00bfc0e2
kazuki.matsumura <kazuki_matsumura@icloud.com>
2019-11-29 11:06:09 +0000

    gokaku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,10 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>Microposts</title>
+        <title>work log</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
     </head>
 
     <body>
