86f5846ac976e45443cf9183bc7e245d66e2fd3a
kazuki.matsumura <kazuki_matsumura@icloud.com>
2019-11-29 15:10:40 +0000

    user registration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
         <title>work log</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-        <link href="css/style.css" rel="stylesheet" type="text/css">
     </head>
 
     <body>
